c1f3285e534cfba5c4da754f83cfde22d9875e74
ytack <y.t.tack@gmail.com>
2021-10-18 20:24:14 +0900

    Add Tesla K80 (arch=compute_37,code=sm_37) support

--- correlation_package/setup.py
+++ correlation_package/setup.py
@@ -8,6 +8,7 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 cxx_args = ['-std=c++14']
 
 nvcc_args = [
+    '-gencode', 'arch=compute_37,code=sm_37',
     '-gencode', 'arch=compute_50,code=sm_50',
     '-gencode', 'arch=compute_52,code=sm_52',
     '-gencode', 'arch=compute_60,code=sm_60',
